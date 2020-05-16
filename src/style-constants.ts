83e33e17853ecebb6b8afe99082dba7b3442b0de
Adam Alenius <adam.alenius@gmail.com>
2020-05-16 12:30:45 +0200

    update text and styling

---
+++ src/style-constants.ts
@@ -0,0 +1,28 @@
+const colors = {}
+
+const dimensions = {
+  fontSize: {
+    xs: 0.5,
+    s: 0.75,
+    m: 1,
+    l: 1.25,
+    xl: 2,
+    xxl: 3,
+  },
+  headingSizes: {
+    h1: 2.441,
+    h2: 1.953,
+    h3: 1.563,
+    h4: 1.25,
+  },
+  lineHeight: {
+    regular: 1.45,
+    heading: 1.2,
+  },
+  containerPadding: {
+    small: 1,
+    regular: 1.5,
+  },
+}
+
+export { colors, dimensions }
